e7dc7ef49615ebb694919a806b60991c82ddac49
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-12-09 15:00:50 +0530

    add Mongoose user model with schema definition and JSON transformation

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -25,6 +25,13 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+userSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  },
+});
 const User = mongoose.model("User", userSchema);
 
 export default User;
