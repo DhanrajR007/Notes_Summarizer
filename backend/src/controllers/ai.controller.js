39d6e4705129d786788a0fb86d8a1cde9045e3bb
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-12-10 16:32:28 +0530

    Add AI summarization endpoint with Google GenAI integration for text and PDF.

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -2,17 +2,18 @@ import { generateSummary } from "../services/ai.service.js";
 
 export const summary = async (req, res) => {
   try {
-    const text = req.body;
-    const file = req.file;
-    console.log(file);
-    // if (!text) {
-    //     throw new BadRequestError("Text is required");
-    // }
-    // const summary = await generateSummary(text);
+    const { text } = req.body || null;
+    const file = req.file || null;
+
+    if (!text && !file) {
+      throw new BadRequestError("Text or file is required");
+    }
+
+    const summary = await generateSummary(file, text);
     return res.status(200).json({
       success: true,
       message: "Summary generated successfully",
-      file,
+      summary,
     });
   } catch (error) {
     throw error;
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,11 +1,31 @@
 import { GoogleGenAI } from "@google/genai";
-
 const ai = new GoogleGenAI({});
+export const generateSummary = async (file, text) => {
+  console.log(file, text);
+  const contents = [];
 
-export async function generateSummary(text) {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: text,
-  });
-  return response.text;
-}
+  if (file) {
+    // PDF upload hua
+    contents.push({ text: "Summarize this document" }); // instruction
+    contents.push({
+      inlineData: {
+        mimeType: "application/pdf",
+        data: file.buffer.toString("base64"),
+      },
+    });
+  } else if (text) {
+    // Direct text
+    contents.push({ text: text });
+  }
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: contents,
+    });
+
+    return { summary: response.text };
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
